99e4728e3fd8759652f7d6e4a61d08a460242ec6
Daniel Bergholz <daniel@maratonas.academy>
2021-09-16 17:42:25 -0300

    feat: add swagger to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import helmet from 'fastify-helmet';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -15,6 +21,45 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.register(helmet);
   app.enableCors({ origin: process.env.CORS_ORIGIN || '*' });
+
+  app.register(helmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Dribble API')
+    .setDescription(
+      'Documentação do backend do Dribble, utilizado no Formação Frontend Expert',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .setContact(
+      'Daniel Berg',
+      'https://maratonas.academy/',
+      'daniel@maratonas.academy',
+    )
+    .setExternalDoc(
+      'GitHub do backend',
+      'https://github.com/daniel-bergholz/dribble-backend',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+
+  SwaggerModule.setup('docs', app, document, customOptions);
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 
